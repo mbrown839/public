0c8cd0d149e39a1263176c5e1179d6797408b89c
mbrown839 <mattbrown87@hotmail.co.uk>
2016-02-24 22:37:09 +0000

    Update nhsNumberCheckBool.swift

--- nhsNumberCheckBool.swift
+++ nhsNumberCheckBool.swift
@@ -2,31 +2,39 @@ import Foundation
 
 func nhsNumberCheckBool(nhsNumber: String) -> Bool {
 	// returns a bool corresponding to "is this a valid NHS number?" 
-	// See http://www.datadictionary.nhs.uk/version2/data_dictionary/data_field_notes/n/nhs_number_de.asp?shownav=0	
+	// See http://www.datadictionary.nhs.uk/version2/data_dictionary/data_field_notes/n/nhs_number_de.asp?shownav=0
+	
 	// checks that nhsNumber actually contains something
 	if nhsNumber.isEmpty {
 		return false
 	}
+	
     	// sets up nhsNum as a separate variable
     	var nhsNum:String = nhsNumber
+    	
     	// removes any dashes or spaces
     	nhsNum = String(nhsNum.characters.filter {$0 != " " && $0 != "-"})
+    	
     	// checks that there are exactly 10 characters
     	if nhsNum.characters.count != 10 {
 	    return false
     	}
+    	
     	// need to declare this as it is an optional? Checks it only contains numbers.
 	let nhsInt:Int? = Int(nhsNum)
 	if nhsInt == nil {
 		return false
 	}
+	
     	// removes the last digit
 	let rangeCheck = Int(nhsInt! / 10)
+	
 	// check that NHS number is in range, see http://systems.hscic.gov.uk/nhsnumber/staff/comms/4788factsheet.pdf
     	if (rangeCheck < 400000000 || rangeCheck >= 800000000
     		|| (rangeCheck >= 500000000 && rangeCheck < 600000000)) {
 	    return false
 	}
+	
 	// calculates the final check sum before we recycle the rangeCheck variable
     	let providedCs:Int = nhsInt! - 10*rangeCheck
 
@@ -38,7 +46,6 @@ func nhsNumberCheckBool(nhsNumber: String) -> Bool {
     	// having initialised  tot as 0, then working back from the last digit
 	// before the check sum, multiplies by 10 and adds to the total, multiplies 
 	// by 9 and adds to the total and so on for all digits.
-	
     	while j < 10 {
         	tot += multiplier * (rangeCheckIterate - 10*(Int(rangeCheckIterate/10)))
         	rangeCheckIterate = Int(rangeCheckIterate/10)
@@ -64,13 +71,13 @@ var z: Int = 0
 var nhs: Int = 4160000000
 var nhsChar: String
 
+// starts the timer
 let date_start = NSDate()
 
 // loops from z which is 0 by default to some number, calculating if the number
 // is valid, returning a bool then adding 1 to both z and the nhs variable
 // by this method all numbers in the starting nhs value to nhs + some number
 // will be checked for validity
-
 while z < 64100000 {
 	nhsChar = String(nhs)
 	// these bits are for testing, leave as comments by default
@@ -81,4 +88,5 @@ while z < 64100000 {
 	nhs+=1
 }
 
+// prints the time elapsed since the timer began
 print("\(-date_start.timeIntervalSinceNow)")
