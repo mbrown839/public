4de7be306386d997150219db6b12c1ac079d7f2d
mbrown839 <mattbrown87@hotmail.co.uk>
2016-06-17 13:28:41 +0100

    Update nhsNumberCheckBool.swift

--- nhsNumberCheckBool.swift
+++ nhsNumberCheckBool.swift
@@ -10,74 +10,74 @@ func nhsNumberCheckBool(nhsNumber: String) -> Bool {
 	
 	// checks that nhsNumber actually contains something
 	//  using guard not empty, remove spaces & dashes, guard = 10 to allow less strict arguments.
-    guard nhsNumber.isEmpty == false else {
-        return false
-    }
-    
-    // sets up nhsNum as a separate var, not let since it can be modified
-    var nhsNum:String = nhsNumber
-    
-    // removes any dashes or spaces
-    nhsNum = String(nhsNum.characters.filter {$0 != " " && $0 != "-"})
-    
-    // checks that the number contains exactly 10 characters
-    guard nhsNum.characters.count == 10 else {
-        return false
-    }
-    
-    // need to declare this as it is an optional? Checks it only contains numbers.
-    let nhsInt:Int? = Int(nhsNum)
-    
+	guard nhsNumber.isEmpty == false else {
+		return false
+	}
+	
+	// sets up nhsNum as a separate var, not let since it can be modified
+	var nhsNum:String = nhsNumber
+	
+	// removes any dashes or spaces
+	nhsNum = String(nhsNum.characters.filter {$0 != " " && $0 != "-"})
+	
+	// checks that the number contains exactly 10 characters
+	guard nhsNum.characters.count == 10 else {
+		return false
+	}
+	
+	// need to declare this as it is an optional? Checks it only contains numbers.
+	let nhsInt:Int? = Int(nhsNum)
+	
 	// Checks it only contains numbers.
-    guard nhsInt != nil else {
-        return false
-    }
-    
-    // removes the last digit
-    let rangeCheck = Int(nhsInt! / 10)
-    
-    // check that NHS number is in range, see http://systems.hscic.gov.uk/nhsnumber/staff/comms/4788factsheet.pdf
-    guard rangeCheck >= 400000000 && rangeCheck < 800000000
-    	&& (rangeCheck < 500000000 || rangeCheck >= 600000000) else {
-            return false
-    }
-    
-    // calculates the final check sum before we recycle the rangeCheck variable
-    let providedCs:Int = nhsInt! - 10*rangeCheck
-    
-    var tot:Int = 0
-    var rangeCheckIterate:Int = rangeCheck
-    var j:Int = 1
-    var multiplier:Int = 10
-    
-    /* 
-    *  Having initialised tot as 0, then working back from the last digit
-    *  before the check sum, multiplies by 10 and adds to the total, multiplies 
-    *  by 9 and adds to the total and so on for all digits.
-    */
-    
-    while j < 10 {
-        tot += multiplier * (rangeCheckIterate - 10*(Int(rangeCheckIterate/10)))
-        rangeCheckIterate = Int(rangeCheckIterate/10)
-        j+=1
-        multiplier-=1
-    }
-    
-    // computes tot(mod 11) then takes this from 11
-    var m:Int = 11 - (tot % 11)
-    
-    // if m is 10, this NHS number is not valid. If m is 11, sets m to 10
-    switch m {
-    case 10:
-        return false
-    case 11:
-        m = 0
-    default:
-        ()
-    }
-    
-    // returns whether or not m matches providedCs
-    return(m == providedCs)
+	guard nhsInt != nil else {
+		return false
+	}
+	
+	// removes the last digit
+	let rangeCheck = Int(nhsInt! / 10)
+	
+	// check that NHS number is in range, see http://systems.hscic.gov.uk/nhsnumber/staff/comms/4788factsheet.pdf
+	guard rangeCheck >= 400000000 && rangeCheck < 800000000
+		&& (rangeCheck < 500000000 || rangeCheck >= 600000000) else {
+			return false
+	}
+	
+	// calculates the final check sum before we recycle the rangeCheck variable
+	let providedCs:Int = nhsInt! - 10*rangeCheck
+	
+	var tot:Int = 0
+	var rangeCheckIterate:Int = rangeCheck
+	var j:Int = 1
+	var multiplier:Int = 10
+	
+	/* 
+	*  Having initialised tot as 0, then working back from the last digit
+	*  before the check sum, multiplies by 10 and adds to the total, multiplies 
+	*  by 9 and adds to the total and so on for all digits.
+	*/
+	
+	while j < 10 {
+		tot += multiplier * (rangeCheckIterate - 10*(Int(rangeCheckIterate/10)))
+		rangeCheckIterate = Int(rangeCheckIterate/10)
+		j+=1
+		multiplier-=1
+	}
+	
+	// computes tot(mod 11) then takes this from 11
+	var m:Int = 11 - (tot % 11)
+	
+	// if m is 10, this NHS number is not valid. If m is 11, sets m to 10
+	switch m {
+	case 10:
+		return false
+	case 11:
+		m = 0
+	default:
+		()
+	}
+	
+	// returns whether or not m matches providedCs
+	return(m == providedCs)
 }
 
 var z: Int = 0
@@ -95,13 +95,13 @@ let date_start = NSDate()
 */
 
 while z < 1000 {
-    nhsChar = String(nhs)
-    // these bits are for testing, leave as comments by default
-    // print(nhsChar)
+	nhsChar = String(nhs)
+	// these bits are for testing, leave as comments by default
+	// print(nhsChar)
 	print(nhsNumberCheckBool(nhsNumber: nhsChar))
 	// nhsNumberCheckBool(nhsNumber: nhsChar)
-    z+=1
-    nhs+=1
+	z+=1
+	nhs+=1
 }
 
 // prints the time elapsed since the timer began
